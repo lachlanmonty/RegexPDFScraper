90343bf6d301c84d1d242745855c041a86aa8392
lachlanmonty <montylachlan@gmail.com>
2022-09-26 13:32:46 +1000

    update

--- RegexPDFScraper/rps_gui_main.py
+++ RegexPDFScraper/rps_gui_main.py
@@ -1,12 +1,8 @@
 from __future__ import with_statement
-import numpy as np
-import sys
 import os
 import pandas as pd
 import fitz
-
 from rps_gui import Ui_RPS_GUI
-
 
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
@@ -16,6 +12,8 @@ class MainWindow(qtw.QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.file_names = None
+        self.df_o = None
         self.ui = Ui_RPS_GUI()
         self.ui.setupUi(self)
 
@@ -23,7 +21,7 @@ class MainWindow(qtw.QMainWindow):
 
         self.ui.regex_search_button.clicked.connect(self.regex_search)
         self.ui.save_button.clicked.connect(self.save_data_as_xlsx)
-        
+
         # self.ui.save_button.clicked.connect(self.show_dialog)
 
     def regex_search(self):
@@ -38,7 +36,7 @@ class MainWindow(qtw.QMainWindow):
             df_match["file_path"] = file
             df_match["file_name"] = os.path.basename(os.path.normpath(file))
             return df_match
-        
+
         # loop the get_text function
         def loop_get_text(filenames, pattern):
             df = []
@@ -48,14 +46,12 @@ class MainWindow(qtw.QMainWindow):
             columns = {"level_0": "page_no"}
             df = df.rename(columns=columns)
             return df
-        
+
         try:
             self.df_o = loop_get_text(self.file_names, self.ui.regex_input.text())
             self.ui.regex_confirm.setText(f"{len(self.df_o)} match/s found")
         except:
-            self.ui.regex_confirm.setText(f"No matches found")
-            
-
+            self.ui.regex_confirm.setText("No matches found")
 
     # def select_regex(self):
     #     # open file dialog
@@ -66,7 +62,6 @@ class MainWindow(qtw.QMainWindow):
         # open file dialog
         self.file_names, _ = qtw.QFileDialog.getOpenFileNames(self, "Open File", "", "pdf (*.pdf)")
         self.ui.file_confirm.setText(f"{str(len(self.file_names))} file/s selected")
-
 
     def save_data_as_xlsx(self):
         name = qtw.QFileDialog.getSaveFileName(self, "Save File", filter="*.xlsx")
